558350077f37bff6f61d6c844cc7e5900a59b468
Joe Fraser <joefraser7@gmail.com>
2025-04-08 16:28:38 -0500

    Build an API for creating inventory

---
+++ app/api/inventory/route.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from "next/server";
+import { z } from "zod";
+import prisma from "@/prisma/client";
+const createInventorySchema = z.object({
+  name: z.string().min(1).max(255),
+  description: z.string().min(1),
+});
+
+export async function POST(request: NextRequest) {
+  const body = await request.json();
+  const validation = createInventorySchema.safeParse(body);
+  if (!validation.success)
+    return NextResponse.json(validation.error.errors, { status: 400 });
+
+  const newInventory = await prisma.inventory.create({
+    data: { name: body.name, description: body.description },
+  });
+
+  return NextResponse.json(newInventory, { status: 201 });
+}
