ac9b38b8fe298e71f2f6bf830a9f11417fb4b1c7
Joe Fraser <joefraser7@gmail.com>
2025-04-09 14:39:37 -0500

    Setup Radix UI

--- app/inventory/page.tsx
+++ app/inventory/page.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const InventoryPage = () => {
-  return <div>InventoryPage</div>;
+  return (
+    <div>
+      <Button>Update Inventory</Button>
+    </div>
+  );
 };
 
 export default InventoryPage;
