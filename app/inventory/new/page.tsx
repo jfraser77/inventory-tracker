7b62fb1b8a1df6e1df74c0c72798772a7e5db37e
Joe Fraser <joefraser7@gmail.com>
2025-04-28 15:42:53 -0500

    Handle form submission

--- app/inventory/new/page.tsx
+++ app/inventory/new/page.tsx
@@ -1,17 +1,40 @@
 "use client";
 
 import { Button, TextField } from "@radix-ui/themes";
-
 import SimpleMDE from "react-simplemde-editor";
+import { useForm, Controller } from "react-hook-form";
+import axios from "axios";
 import "easymde/dist/easymde.min.css";
+import { useRouter } from "next/navigation";
+
+interface InventoryForm {
+  name: string;
+  description: string;
+}
 
 const NewInventoryPage = () => {
+  const router = useRouter();
+  const { register, control, handleSubmit } = useForm<InventoryForm>();
+
   return (
-    <div className="max-w-xl space-y-3">
-      <TextField.Root placeholder="Name"></TextField.Root>
-      <SimpleMDE placeholder="Description" />
+    <form
+      className="max-w-xl space-y-3"
+      onSubmit={handleSubmit(async (data) => {
+        axios.post("/api/inventory", data);
+        router.push("/inventory");
+      })}
+    >
+      <TextField.Root placeholder="Name" {...register("name")}></TextField.Root>
+      <Controller
+        name="description"
+        control={control}
+        render={({ field }) => (
+          <SimpleMDE placeholder="Description" {...field} />
+        )}
+      />
+
       <Button>Submit New Inventory</Button>
-    </div>
+    </form>
   );
 };
 
